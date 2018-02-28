7013387b7c558610756ba935389f397dd2b4cfe5
siavash abdoli <siavash.abdoli@gmail.com>
2018-02-28 12:27:47 +0330

    FireBase Services created

---
+++ app/src/main/java/xyz/siavash/storygame/infrastructure/MyFirebaseInstanceIdService.java
@@ -0,0 +1,30 @@
+package xyz.siavash.storygame.infrastructure;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Siavash on 2/28/18.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+  private static final String TAG = MyFirebaseInstanceIdService.class.getSimpleName();
+
+  @Override
+  public void onTokenRefresh() {
+    // Get updated InstanceID token.
+    String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+    Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    // If you want to send messages to this application instance or
+    // manage this apps subscriptions on the server side, send the
+    // Instance ID token to your app server.
+    sendRegistrationToServer(refreshedToken);
+  }
+
+  private void sendRegistrationToServer(String refreshedToken) {
+
+  }
+}
